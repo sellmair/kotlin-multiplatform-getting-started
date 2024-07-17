620ec628619772fd9dbac0541823c823cc07ef93
Sebastian Sellmair <sebastian.sellmair@jetbrains.com>
2024-07-17 21:09:42 +0200

    Add initial, empty, unconfigured Xcode project

---
+++ app/iosApp/kmpSample/kmpSample/kmpSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  kmpSampleApp.swift
+//  kmpSample
+//
+//  Created by Sebastian Sellmair on 17.07.24.
+//
+
+import SwiftUI
+
+@main
+struct kmpSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
